b06bc7d98ae63ad04ce112abfb35bbab78897350
jhd7130 <jhd7130@github.com>
2022-04-15 00:37:37 +0900

    first

---
+++ src/test/java/com/flab/fire_inform/FireInformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.fire_inform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireInformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
