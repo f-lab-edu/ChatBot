b94c80b54e862b8739bfd1532e75f0ffb4ceabc5
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2022-04-18 03:15:34 +0900

    Modify Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,18 +19,18 @@ pipeline {
 //             }
 //         }
 
-        stage('Transfer') {
-            steps {
-                sh 'sshpass -p $DEPLOY_SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/flab/build/libs/fire_inform-0.0.1-SNAPSHOT.jar $DEPLOY_SERVER_ID@106.10.59.248:build'
-            }
-        }
-
-//         stage('Connect Deploy Server') {
+//         stage('Transfer') {
 //             steps {
-//                 sh 'sshpass -p $DEPLOY_SERVER_PASSWORD ssh -T -p 12308 $DEPLOY_SERVER_ID@106.10.59.248'
-//                 sh 'cd build'
-//                 sh 'nohup java -jar fire_inform-0.0.1-SNAPSHOT.jar &'
+//                 sh 'sshpass -p $DEPLOY_SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/flab/build/libs/fire_inform-0.0.1-SNAPSHOT.jar $DEPLOY_SERVER_ID@106.10.59.248:build'
 //             }
 //         }
+
+        stage('Connect Deploy Server') {
+            steps {
+                sh 'sshpass -p $DEPLOY_SERVER_PASSWORD ssh -T -p 12308 $DEPLOY_SERVER_ID@106.10.59.248'
+                sh 'cd build'
+                sh 'nohup java -jar fire_inform-0.0.1-SNAPSHOT.jar &'
+            }
+        }
     }
 }
