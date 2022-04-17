852c8adf8f007e3ecfaccbd7ead02a5f687b5d78
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2022-04-18 04:13:27 +0900

    Modify Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -39,7 +39,7 @@ pipeline {
 
         stage('Deploy') {
             steps {
-                sh 'nohup java -jar build/fire_inform-0.0.1-SNAPSHOT.jar &'
+                sh 'sudo nohup java -jar build/fire_inform-0.0.1-SNAPSHOT.jar &'
             }
         }
     }
