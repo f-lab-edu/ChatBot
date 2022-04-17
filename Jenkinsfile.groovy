5fc97022d1665e39a436e53981eb90e98245b2e5
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2022-04-18 01:01:36 +0900

    Add Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,11 +1,27 @@
 node {
-    stage('Git clone') {
-      steps {
-        git credentialsId: 'cbf50f14-c18f-4bc0-a792-668780641040',
-        url: 'https://github.com/f-lab-edu/Fire-inform'
-      }
+//     stage('Git clone') {
+//       steps {
+//         git credentialsId: 'cbf50f14-c18f-4bc0-a792-668780641040',
+//         url: 'https://github.com/f-lab-edu/Fire-inform'
+//       }
+//     }
+//     stage('Build') {
+//         sh "./gradlew clean build"
+//     }
+
+    stage('Ready') {
+        echo "Ready"
     }
+
     stage('Build') {
-        sh "./gradlew clean build"
+        echo "Build"
+    }
+
+    stage('Transfer') {
+        echo "Transfer"
+    }
+
+    stage('Run') {
+        echo "Run"
     }
 }
