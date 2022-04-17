9f47ffa16154e200efd20c6430c5d4093d1c05b7
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2022-04-18 02:28:58 +0900

    Modify Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,4 @@
 pipeline {
-//     stage('Git clone') {
-//       steps {
-//         git credentialsId: 'cbf50f14-c18f-4bc0-a792-668780641040',
-//         url: 'https://github.com/f-lab-edu/Fire-inform'
-//       }
-//     }
-//     stage('Build') {
-//         sh "./gradlew clean build"
-//     }
-
     agent any
     environment {
         SERVER_PASSWORD = credentials('serverPassword')
@@ -30,8 +20,7 @@ pipeline {
 
         stage('Transfer') {
             steps {
-                echo "$SERVER_PASSWORD"
-                sh "sshpass -p $SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no -T /var/lib/jenkins/workspace/flab/build/libs/fire_inform-0.0.1-SNAPSHOT.jar root@106.10.59.248:build"
+                sh "sshpass -p $SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/flab/build/libs/fire_inform-0.0.1-SNAPSHOT.jar root@106.10.59.248:build"
             }
         }
 
