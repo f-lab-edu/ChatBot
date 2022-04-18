0616b760de6bbb00ce98c479619b57db673f0f51
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2022-04-18 17:43:14 +0900

    Modify Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,20 +19,16 @@ pipeline {
 //             }
 //         }
 
-//         stage('Send Jar') {
-//             steps {
-//                 sh 'sshpass -p $DEPLOY_SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/flab/build/libs/fire_inform-0.0.1-SNAPSHOT.jar $DEPLOY_SERVER_ID@106.10.59.248:build'
-//             }
-//         }
+        stage('Send Jar') {
+            steps {
+                sh 'sshpass -p $DEPLOY_SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/flab/build/libs/fire_inform-0.0.1-SNAPSHOT.jar $DEPLOY_SERVER_ID@106.10.59.248:build'
+                sh 'sshpass -p $DEPLOY_SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/flab/deploy.sh $DEPLOY_SERVER_ID@106.10.59.248:build'
+            }
+        }
 
         stage('Connect Deploy Server') {
             steps {
-                sh 'sshpass -p $DEPLOY_SERVER_PASSWORD ssh -T -p 12308 $DEPLOY_SERVER_ID@106.10.59.248'
-
-                sh 'pwd'
-                sh 'kill -9 "pgrep -f java"'
-                sh 'sleep 5'
-                sh 'nohup java -jar build/fire_inform-0.0.1-SNAPSHOT.jar &'
+                sh 'sshpass -p $DEPLOY_SERVER_PASSWORD ssh -T -p 12308 $DEPLOY_SERVER_ID@106.10.59.248 sh build/deploy.sh'
             }
         }
     }
---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+pwd
+
+pid="$(pgrep -f java)";
+
+if [ "$pid" != ""]; then
+    kill -9 $pid;
+    echo "$pid process kill complete"
+else
+    echo "pid is empty"
+fi
+
+sleep 5
+
+nohup java -jar build/fire_inform-0.0.1-SNAPSHOT.jar &
+
+exit 0
