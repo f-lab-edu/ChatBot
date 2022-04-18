c16b37dcc18dec57270fe1b18edb22de075d63ae
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2022-04-18 17:55:45 +0900

    Modify Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,28 +13,28 @@ pipeline {
             }
         }
 
-        stage('Build') {
-            steps {
-                sh "./gradlew clean build"
-            }
-        }
-
-        stage('Send Jar') {
-            steps {
-                sh 'sshpass -p $DEPLOY_SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/flab/build/libs/fire_inform-0.0.1-SNAPSHOT.jar $DEPLOY_SERVER_ID@106.10.59.248:build'
-            }
-        }
-
-        stage('Send Shell Script') {
-            steps {
-                sh sh 'sshpass -p $DEPLOY_SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/flab/deploy.sh $DEPLOY_SERVER_ID@106.10.59.248:build'
-            }
-        }
-
-        stage('Connect Deploy Server') {
-            steps {
-                sh 'sshpass -p $DEPLOY_SERVER_PASSWORD ssh -T -p 12308 $DEPLOY_SERVER_ID@106.10.59.248 sh build/deploy.sh'
-            }
-        }
+//         stage('Build') {
+//             steps {
+//                 sh "./gradlew clean build"
+//             }
+//         }
+//
+//         stage('Send Jar') {
+//             steps {
+//                 sh 'sshpass -p $DEPLOY_SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/flab/build/libs/fire_inform-0.0.1-SNAPSHOT.jar $DEPLOY_SERVER_ID@106.10.59.248:build'
+//             }
+//         }
+//
+//         stage('Send Shell Script') {
+//             steps {
+//                 sh 'sshpass -p $DEPLOY_SERVER_PASSWORD scp -P 12308 -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/flab/deploy.sh $DEPLOY_SERVER_ID@106.10.59.248:build'
+//             }
+//         }
+//
+//         stage('Connect Deploy Server') {
+//             steps {
+//                 sh 'sshpass -p $DEPLOY_SERVER_PASSWORD ssh -T -p 12308 $DEPLOY_SERVER_ID@106.10.59.248 sh build/deploy.sh'
+//             }
+//         }
     }
 }
