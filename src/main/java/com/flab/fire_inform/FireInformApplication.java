8c9930affe35b72d2299cac2bae0fb36a5b0b85e
jhd7130 <jhd7130@github.com>
2022-04-27 15:52:47 +0900

    Mybatis sqlSessionFactory Bean Loacation change

--- src/main/java/com/flab/fire_inform/FireInformApplication.java
+++ src/main/java/com/flab/fire_inform/FireInformApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@SpringBootApplication
 public class FireInformApplication {
 
 	public static void main(String[] args) {
