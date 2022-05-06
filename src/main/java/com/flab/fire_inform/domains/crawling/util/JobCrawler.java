9b210ba98ed6a2fe65331ffb00149e39cac5e0c9
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2022-05-07 00:02:28 +0900

    crawling 추상메서드 리턴값 수정

--- src/main/java/com/flab/fire_inform/domains/crawling/util/JobCrawler.java
+++ src/main/java/com/flab/fire_inform/domains/crawling/util/JobCrawler.java
@@ -1,10 +1,6 @@
 package com.flab.fire_inform.domains.crawling.util;
 
-import com.flab.fire_inform.domains.crawling.entity.Recruit;
-
-import java.util.List;
-
 public interface JobCrawler {
 
-    public List<Recruit> crawling();
+    void crawling();
 }
