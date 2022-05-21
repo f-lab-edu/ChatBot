d4a2ff7ee075aef49206e4ae431572f90369826d
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2022-05-21 18:26:31 +0900

    clone할 branch 변경

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,7 +20,7 @@ pipeline {
 
         stage("Checkout") {
             steps {
-                git url: "https://github.com/f-lab-edu/ChatBot.git", branch: "jenkins",
+                git url: "https://github.com/f-lab-edu/ChatBot.git", branch: "main",
                 credentialsId: "cbf50f14-c18f-4bc0-a792-668780641040"
             }
         }
